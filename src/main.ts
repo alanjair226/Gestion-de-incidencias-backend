9473865266aaca43bfa8593645a1b4cca9409e4c
Alan Almaguer <alalmag2@gmail.com>
2024-12-30 17:01:15 -0600

    swaggeer

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +16,16 @@ async function bootstrap() {
     })
   );
 
+  const config = new DocumentBuilder()
+    .setTitle("Api Incidencias")
+    .setDescription("Descripción detallada para api de incidencias")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
