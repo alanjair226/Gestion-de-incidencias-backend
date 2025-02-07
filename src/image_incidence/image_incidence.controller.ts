00769b36811a094e7b483387df434f6ffe8a115d
Alan Almaguer <alalmag2@gmail.com>
2025-02-07 16:51:50 -0600

    se agrega la autorización a las imagenes

--- src/image_incidence/image_incidence.controller.ts
+++ src/image_incidence/image_incidence.controller.ts
@@ -2,21 +2,26 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ImageIncidenceService } from './image_incidence.service';
 import { CreateImageIncidenceDto } from './dto/create-image_incidence.dto';
 import { UpdateImageIncidenceDto } from './dto/update-image_incidence.dto';
+import { Auth } from '../auth/decorators/auth.decorator';
+import { Role } from '../common/enum/rol.enum';
 
 @Controller('image-incidence')
 export class ImageIncidenceController {
   constructor(private readonly imageIncidenceService: ImageIncidenceService) {}
 
+  @Auth([Role.ADMIN, Role.SUPERADMIN])
   @Post()
   create(@Body() createImageIncidenceDto: CreateImageIncidenceDto) {
     return this.imageIncidenceService.create(createImageIncidenceDto);
   }
 
+  @Auth([Role.USER])
   @Get()
   findAll() {
     return this.imageIncidenceService.findAll();
   }
 
+  @Auth([Role.USER])
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.imageIncidenceService.findOne(+id);
@@ -28,6 +33,7 @@ export class ImageIncidenceController {
     return {message:"to be developed"}
   }
 
+  @Auth([Role.ADMIN, Role.SUPERADMIN])
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.imageIncidenceService.remove(+id);
